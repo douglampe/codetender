966fc91b706d41a06486965787aedda1bcc3742f
Doug Lampe <lampedoug@yahoo.com>
2023-08-15 09:04:09 -0400

    chore: update config order

--- src/config/CodeTenderConfig.ts
+++ src/config/CodeTenderConfig.ts
@@ -1,21 +1,21 @@
 import { IQuestionReader, RemoteTemplateConfig, ScriptsConfig, Token, Variable } from '../index';
 
 export interface CodeTenderConfig {
-  folder: string;
   template?: string;
+  folder: string;
   file?: string;
-  logger?: (message: any) => void;
   tokens?: Array<Token>;
   remote?: Array<RemoteTemplateConfig>;
   noReplace?: Array<string>;
   ignore?: Array<string>;
   delete?: Array<string>;
-  banner?: string | Array<string>;
   overwrite?: boolean;
   scripts?: ScriptsConfig;
   variables?: Array<Variable>;
   noSplash?: boolean;
-  quiet?: boolean;
+  banner?: string | Array<string>;
   verbose?: boolean;
+  quiet?: boolean;
+  logger?: (message: any) => void;
   readerFactory?: () => IQuestionReader;
 }
